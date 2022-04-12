a19f6ff28d0b6410fcf06f1c038ac186d3b3d976
sn1NKS <karetnikov87@gmail.com>
2022-04-12 10:20:45 +0300

    creat program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3-м символам.
+
+string[] array = {"hello", "2", "world", ":-)"};
+string[] array2 = {"hello", "2", "world", ":-)"};
+int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i].Length <= 3)
+   {
+      array2[index]= array[i];
+      index++;
+      Console.Write(array2[i] + " ");
+   }
+} 
